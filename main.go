0404c0d92b7d73ecbdf85357e1870ceb29066912
saitooooooo <kitsit.39@gmail.com>
2022-06-27 21:57:38 +0900

    Update: get list s3 buckets

--- main.go
+++ main.go
@@ -1,50 +1,74 @@
 package main
 
 import (
-	"flag"
+	"context"
 	"fmt"
-	"os"
-	"time"
+	"log"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/manifoldco/promptui"
 )
 
-var (
-	argRegion = flag.String("region", "ap-northeast-1", "specify region name")
-	argProfile = flag.String("profile", "", "specify credential profile name")
-	argBucket = flag.String("bucket", "", "specify bucket name")
-	argPrefix = flag.String("prefix", "", "specify object key prefix")
-)
+type S3ListBucketsAPI interface {
+	ListBuckets(ctx context.Context,
+		params *s3.ListBucketsInput,
+		optFns ...func(*s3.Options)) (*s3.ListBucketsOutput, error)
+}
+
+func chooseValueFromPrompt(l string, d string) string {
+	prompt := promptui.Prompt{
+		Label:   l,
+		Default: d,
+	}
+	v, err := prompt.Run()
+	if err != nil {
+		log.Fatalf("Prompt failed %v\n", err)
+	}
+	return v
+}
+
+func chooseValueFromPromptItems(l string, i []string) string {
+	prompt := promptui.Select{
+		Label: l,
+		Items: i,
+	}
+	_, v, err := prompt.Run()
+	if err != nil {
+		log.Fatalf("Prompt failed %v\n", err)
+	}
+
+	return v
+}
+
+func GetAllBuckets(c context.Context, api S3ListBucketsAPI, input *s3.ListBucketsInput) (*s3.ListBucketsOutput, error) {
+	return api.ListBuckets(c, input)
+}
 
 func main() {
-	flag.Parse()
-	defer func() {
-		if r := recover(); r != nil {
-			flag.Usage()
-			fmt.Println(r)
-			os.Exit(1)
-		}
-	}()
+	aws_region := chooseValueFromPrompt("Please enter aws region(Default: ap-northeast-1)", "ap-northeast-1")
+	aws_profile := chooseValueFromPrompt("Please enter aws profile(If empty, default settings are loaded)", "")
 
-	config := aws.Config{Region: argRegion, MaxRetries: aws.Int(10)}
-	if *argProfile != "" {
-		creds := credentials.NewSharedCredentials("", *argProfile)
-		config.Credentials = creds
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(aws_region), config.WithSharedConfigProfile(aws_profile))
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
 	}
-	sess := session.New(&config)
-	svc := s3.New(sess)
 
-	params := &s3.ListObjectsV2Input{Bucket: argBucket, Prefix: argPrefix}
-	jst, _ := time.LoadLocation("Asia/Tokyo")
-	svc.ListObjectsV2Pages(params,
-		func(page *s3.ListObjectsV2Output, lastPage bool) bool {
-			for _, obj := range page.Contents {
-				fmt.Printf("%s %10d %s\n", obj.LastModified.In(jst).Format("2006-01-02"), *obj.Size, *obj.Key)
-			}
-			return *page.IsTruncated
-		})
+	client := s3.NewFromConfig(cfg)
+
+	input := &s3.ListBucketsInput{}
+
+	result, err := GetAllBuckets(context.TODO(), client, input)
+	if err != nil {
+		fmt.Println("Got an error retrieving buckets:")
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Buckets:")
+
+	for _, bucket := range result.Buckets {
+		fmt.Println(*bucket.Name + ": " + bucket.CreationDate.Format("2006-01-02 15:04:05 Monday"))
+	}
 }
 
