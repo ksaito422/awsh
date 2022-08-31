b6b9b68c2f30a12d0074000a8b1b729562eb4d50
saitooooooo <kitsit.39@gmail.com>
2022-08-31 22:26:51 +0900

    Add: stop task

---
+++ pkg/ecs/stop_task.go
@@ -0,0 +1,38 @@
+package ecs
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ecs"
+)
+
+type ECSStopTaskAPI interface {
+	StopTask(ctx context.Context,
+		params *ecs.StopTaskInput,
+		optFns ...func(*ecs.Options)) (*ecs.StopTaskOutput, error)
+}
+
+func stopTaskAPI(c context.Context, api ECSStopTaskAPI, input *ecs.StopTaskInput) (*ecs.StopTaskOutput, error) {
+	return api.StopTask(c, input)
+}
+
+// aws ecs stop-task
+func StopTask(cfg aws.Config, cluster, taskArn string) {
+	client := ecs.NewFromConfig(cfg)
+	input := &ecs.StopTaskInput{
+		Cluster: &cluster,
+		Task:    &taskArn,
+	}
+
+	_, err := stopTaskAPI(context.TODO(), client, input)
+	if err != nil {
+		fmt.Println("Got an error retrieving task:")
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Done. Bye!")
+}
